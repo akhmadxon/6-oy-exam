1c8c46c7bf13a0aaaec3bc99ed87b5e1b560c72e
Your Name <you@example.com>
2024-06-10 00:44:33 +0500

    first commit

---
+++ src/router/index.jsx
@@ -0,0 +1,24 @@
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+import App from "../App";
+import Login from "../pages/login/login";
+import Main from "../pages/main/main"
+import Product from "../pages/product/product";
+const Index = () => {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<App/>}>
+            <Route index element={<Login/>}/>
+            <Route path="main/*" element={<Main/>}>
+            <Route path="product" element={<Product/>}/>
+            </Route>
+             </Route>
+        )
+    );
+    return <RouterProvider router={router}/>
+};
+export default Index;
